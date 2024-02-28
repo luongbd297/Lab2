e228ff4f213df1bd5241b6a4e6a618b218e4d475
luongbd297 <155948958+luongbd297@users.noreply.github.com>
2024-02-28 12:52:57 +0700

    Update 404Page.cshtml.cs

--- ecomerce/Pages/404Page.cshtml.cs
+++ ecomerce/Pages/404Page.cshtml.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace ecomerce.Pages
 {
-    public class _404PageModel : PageModel
+    public class NotFoundPageModel : PageModel
     {
         public void OnGet()
         {
